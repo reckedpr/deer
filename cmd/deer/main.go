e5525808ce76a022796e94c4dbd4660bef77d76e
reckedpr <103007531+reckedpr@users.noreply.github.com>
2025-10-04 23:13:06 +0100

    add fact endpoint

--- cmd/deer/main.go
+++ cmd/deer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"math/rand"
 	"net/http"
 	"os"
@@ -10,9 +11,14 @@ import (
 )
 
 var deerList []Deer
+var factList []Fact
 
 type Deer struct {
 	ImgURL string `json:"img_url,omitempty"`
+}
+
+type Fact struct {
+	Text string `json:"fact"`
 }
 
 // proudly stolen from stackoverflow !
@@ -39,7 +45,13 @@ func getDeer(c *gin.Context) {
 	deer.ImgURL = scheme + "://" + host + deer.ImgURL
 
 	c.IndentedJSON(http.StatusOK, deer)
+}
 
+func getFact(c *gin.Context) {
+	randomIndex := rand.Intn(len(factList))
+	fact := factList[randomIndex]
+
+	c.IndentedJSON(http.StatusOK, fact)
 }
 
 func main() {
@@ -61,11 +73,22 @@ func main() {
 		})
 	}
 
+	factFile, err := os.ReadFile("data/facts.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(factFile, &factList)
+	if err != nil {
+		panic(err)
+	}
+
 	router := gin.Default()
 
 	router.Static("/static/img", "./static/img")
 
 	router.GET("/", getDeer)
+	router.GET("/fact", getFact)
 
 	router.Run("localhost:8080")
 }
