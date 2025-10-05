8322284c32c93e6af0a659210a8e9e0fad9614f7
reckedpr <103007531+reckedpr@users.noreply.github.com>
2025-10-05 22:38:31 +0100

    update img ingest tool

--- rename-img.py
+++ rename-img.py
@@ -9,6 +9,7 @@ import hashlib
 # 50/50 chance it breaks and renames everything hehehe
 
 BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+imgs = ['jpg', 'png', 'jpeg', 'webp']
 
 arg_path = sys.argv[1]
 files = os.listdir(arg_path)
@@ -22,13 +23,17 @@ def file_hash(path):
 
 for file in files:
     path = os.path.join(arg_path, file)
+    
     if not os.path.isfile(path) or file == ".DS_Store":
         continue
+    if file.split(".")[-1] not in imgs:
+        continue
+    
     h_bytes = file_hash(path)
     rng = random.Random(int.from_bytes(h_bytes, "big"))
     
-    name = ''.join(rng.choices(BASE62, k=5))
-    name = arg_path + name + "." + file.split(".")[-1]
+    name = ''.join(rng.choices(BASE62, k=5)) + "." + file.split(".")[-1]
+    name = os.path.join(arg_path, name)
     
     os.rename(path, name)
-    print(f'{file:>20} -> {name}')
+    print(f'{file:>12} -> {name}')
