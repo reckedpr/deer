d637b5d24c49fae2e6b3e0965f2219d76088baf2
103007531+reckedpr@users.noreply.github.com <103007531+reckedpr@users.noreply.github.com>
2026-02-13 21:08:41 +0000

    chore: cleanup web configs

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,15 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
+  build: {
+    assets: '_deer'
   },
-
-  integrations: [react()],
-
-  build: {  assets: 'reckedpr' }
+  vite: {
+      plugins: [tailwindcss()],
+  },
 });
